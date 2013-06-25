b573f7e09826270fbeb66b836575a4b9e30e6594
Ghost hamlet <ghosthamlet@gmail.com>
2013-06-25 23:53:15 +0800

    init

---
+++ src/vv/routes/home.clj
@@ -0,0 +1,15 @@
+(ns vv.routes.home
+  (:use compojure.core)
+  (:require [vv.views.layout :as layout]
+            [vv.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
